8c0cc0bb76a655e3410abd0c79eceefa5b2f14c7
Praniga <manojsrinivasan04@gmail.com>
2025-11-15 15:44:56 +0530

    Configure File

--- terraform_setup/config.py
+++ terraform_setup/config.py
@@ -1,10 +1,9 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'sangameshbhai')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'manojs')
     AWS_REGION = 'us-east-1'
     DYNAMODB_TABLE = 'todo_tasks'
     SNS_TOPIC_ARN = os.getenv("SNS_TOPIC_ARN")
     EMAIL = "manojsrinivasan04@gmail.com"
 
-    
